2ebd1ad4ca8492aab69b4501b49c0ec30f6cfff7
oliverphan <oliver.jlphan@gmail.com>
2025-02-10 00:17:01 -0500

    Ignore vitest in production.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
+    exclude: [...configDefaults.exclude, "dist/**", "node_modules/**"],
     setupFiles: './src/test/setup.ts',
   },
 });
